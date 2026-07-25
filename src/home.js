9357167078e2a6b95fe406c0102e561187b21ca8
emam04bd <emam04bd@gmail.com>
2026-07-25 19:01:02 +0600

    refact: use dedicated module for home content

---
+++ src/home.js
@@ -0,0 +1,24 @@
+export default function () {
+	const contentContainerEl = document.querySelector("#content");
+
+	const navBtnEls = document.querySelectorAll(".nav-btn");
+	navBtnEls.forEach((el) => el.classList.remove("hovering"));
+	navBtnEls[0].classList.add("hovering");
+
+	const homeH1El = document.createElement("h1");
+	homeH1El.classList.add("home");
+	homeH1El.innerText = "Welcome to the RESTAURANT";
+	contentContainerEl.appendChild(homeH1El);
+
+	const homeP1El = document.createElement("p");
+	homeP1El.classList.add("home");
+	homeP1El.innerText =
+		"This is the greatest restaurant which will never exist.";
+	contentContainerEl.appendChild(homeP1El);
+
+	const homeP2El = document.createElement("p");
+	homeP2El.classList.add("home");
+	homeP2El.innerText =
+		"There is nothing else in this tab. Feel free to explore the other tabs";
+	contentContainerEl.appendChild(homeP2El);
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 import "./styles.css";
 
-const contentContainerEl = document.querySelector("#content");
+import showHomeContent from "./home.js";
 
-const navBtnEls = document.querySelectorAll(".nav-btn");
-navBtnEls.forEach((el) => el.classList.remove("hovering"));
-navBtnEls[0].classList.add("hovering");
-
-const homeH1El = document.createElement("h1");
-homeH1El.classList.add("home");
-homeH1El.innerText = "Welcome to the RESTAURANT";
-contentContainerEl.appendChild(homeH1El);
-
-const homeP1El = document.createElement("p");
-homeP1El.classList.add("home");
-homeP1El.innerText = "This is the greatest restaurant which will never exist.";
-contentContainerEl.appendChild(homeP1El);
-
-const homeP2El = document.createElement("p");
-homeP2El.classList.add("home");
-homeP2El.innerText =
-	"There is nothing else in this page. Feel free to explore the other pages";
-contentContainerEl.appendChild(homeP2El);
+showHomeContent();
