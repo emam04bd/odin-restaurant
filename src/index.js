29860b7d1b7773569fdc32abbacea024e8e96d12
emam04bd <emam04bd@gmail.com>
2026-07-27 21:57:35 +0600

    feat: add event listener to each nav button

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import showHomeContent from "./home.js";
 import showAboutContent from "./about.js";
 import showMenuContent from "./menu.js";
 
-showMenuContent();
+const homeBtnEl = document.querySelector("#home-btn");
+const menuBtnEl = document.querySelector("#menu-btn");
+const aboutBtnEl = document.querySelector("#about-btn");
+
+homeBtnEl.addEventListener("click", showHomeContent);
+menuBtnEl.addEventListener("click", showMenuContent);
+aboutBtnEl.addEventListener("click", showAboutContent);
+
+showHomeContent();
